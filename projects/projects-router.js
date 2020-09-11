37d565ce8f1e217fe659475801e7f0e14fc36c04
Joe Bay <jbay55278@gmail.com>
2020-09-11 09:41:29 -0800

    fixed minor issues

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -27,7 +27,7 @@ router.get('/resources', (req, res) => {
     })
     .catch(err => res.status(500).json({ message: err.message }))
 })
-router.get('/:id/tasks', (req, res) => {
+router.get('/:id/tasks', (req, res) => { 
     Projects.getProjectAndTask(req.params.id)
     .then(project => {
         res.status(200).json(project)
